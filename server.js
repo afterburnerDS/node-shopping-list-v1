a195f99fdb260cc37edd78278225a7b79fcdef9f
Daniel Dev <danielgs.silva@gmail.com>
2018-01-13 19:30:02 +0000

    get exercice

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const bodyParser = require('body-parser');
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
 
 // log the http layer
 app.use(morgan('common'));
-
+     
 // we're going to add some items to ShoppingList
 // so there's some data to look at. Note that 
 // normally you wouldn't do this. Usually your
@@ -25,10 +26,19 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('white rice', ['rice', 'whater', 'salter']);
+Recipes.create('cream pie', ['cream', 'pie', 'love']);
+
+
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
